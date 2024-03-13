ff7d04f6d12c8ea70b78442d87331a2585eb7e99
Liam5095 <gamerliam12@outlook.com>
2024-03-13 17:06:37 +0100

    Tools And Armor Fix (Recipes coming soon)

---
+++ src/main/java/net/wickedbog/crazyness/item/custom/ModArmorItem.java
@@ -0,0 +1,11 @@
+package net.wickedbog.crazyness.item.custom;
+
+import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArmorMaterial;
+
+public class ModArmorItem extends ArmorItem {
+
+    public ModArmorItem(ArmorMaterial pMaterial, Type pType, Properties pProperties) {
+        super(pMaterial, pType, pProperties);
+    }
+}
